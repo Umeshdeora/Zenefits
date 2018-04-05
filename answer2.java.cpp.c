d168b719b5c57cbed9f06cad9d220f392923ecc3
Umeshdeora <38092784+Umeshdeora@users.noreply.github.com>
2018-04-05 19:44:46 +0530

    Add files via upload

---
+++ answer2.java.cpp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int n,i;
+n=3;
+char a,b,c;
+for(i=0;i<n;i++)
+    {
+        scanf("%c%c%c\n",a,b,c);
+    }
+    printf("%c%c%c",a,b,c);
+    return 0;
+}
